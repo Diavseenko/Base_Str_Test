f3c106ec97cb2f192d311a37ebcbd682e84bc5d9
DmitrY <diavseenko@yandex.ru>
2022-12-05 16:30:44 +0300

    first test

---
+++ test_main_page.py
@@ -0,0 +1,8 @@
+from pages.main_page import MainPage
+
+
+def test_go_to_login_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(browser, link)
+    page.open()
+    page.get_to_login_page()
