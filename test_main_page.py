27c2784c3be99b54440b784e599b4afbed33cf65
DmitrY <diavseenko@yandex.ru>
2022-12-10 02:17:52 +0300

    two first methods for cart

--- test_main_page.py
+++ test_main_page.py
@@ -1,8 +1,36 @@
 from pages.main_page import MainPage
+from pages.cart_page import CartPage
+import pytest
+import time
 
-
+@pytest.mark.skip
 def test_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
     page = MainPage(browser, link)
     page.open()
     page.get_to_login_page()
+
+@pytest.mark.skip
+def test_login_link_exist(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(browser, link)
+    page.open()
+    page.should_be_login_link()
+
+
+def test_guest_can_see_empty_basket_opened_from_main_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(browser, link)
+    page.open()
+    page.go_to_cart_page()
+    cart_page = CartPage(browser, browser.current_url)
+    cart_page.guest_sees_empty_cart()
+
+
+def test_guest_cant_see_product_in_basket_opened_from_main_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(browser, link)
+    page.open()
+    page.go_to_cart_page()
+    cart_page = CartPage(browser, browser.current_url)
+    cart_page.guest_not_see_product_in_cart()
--- test_product_page.py
+++ test_product_page.py
@@ -1,5 +1,6 @@
 from pages.product_page import ProductPage
 from pages.main_page import MainPage
+from pages.cart_page import CartPage
 import time
 import pytest
 
@@ -56,15 +57,33 @@ def test_message_disappeared_after_adding_product_to_basket(browser):
     product_page.add_product_to_cart()
     product_page.message_disappeared_after_adding_product_to_basket()
 
-
+@pytest.mark.skip
 def test_guest_should_see_login_link_on_product_page(browser):
     link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
     page = ProductPage(browser, link)
     page.open()
     page.should_be_login_link()
-
+@pytest.mark.skip
 def test_guest_can_go_to_login_page_from_product_page(browser):
     link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
     page = ProductPage(browser, link)
     page.open()
     page.get_to_login_page()
+
+
+def test_guest_can_see_an_empty_basket_opened_from_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.go_to_cart_page()
+    cart_page = CartPage(browser, browser.current_url)
+    cart_page.guest_sees_empty_cart()
+
+
+def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.go_to_cart_page()
+    cart_page = CartPage(browser, browser.current_url)
+    cart_page.guest_not_see_product_in_cart()
